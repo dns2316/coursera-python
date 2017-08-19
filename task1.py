125d58c5adfb3aa67f168e4fc01e4954af90b1ce
dns2316 <dns2316@gmail.com>
2017-08-19 23:26:38 +0200

    work code!

--- task1.py
+++ task1.py
@@ -15,12 +15,12 @@ def init_task():
     read_file = data_read_from_txt(input_file)
 
     if read_file != False:
-        num_lines = len(open(input_file).readlines(  ))
+        num_lines = sum(1 for line in read_file)
 
         budget_places_count = int(read_file[0])
-        participants_count = num_lines - 1
+        participants_count = num_lines
 
-        for e in read_file.split('\n')[1:]:
+        for e in read_file.split('\n')[2:]:
             name = []
             evaluation = []
             evaluation_sum = 0
@@ -54,44 +54,32 @@ def init_task():
         # sorted by eval_sum
         answer_out = sorted(answer_before_sort, key=lambda k: k['eval_sum'], reverse=True)
 
-
-
-        # check sum 0 in budget_places. If 0 in sum budget_places = True.
-        zerro_in_sum_bplaces = any(e['eval_sum'] == 0 for e in answer_out[:budget_places_count])
-
-        # count participants without in sum 0.
-        participants_without_zerro = sum(int(e['eval_sum'] == 0) for e in answer_out)
-
         # check tests
-        if len(answer_out) < budget_places_count or zerro_in_sum_bplaces == True or participants_without_zerro >= budget_places_count:
+        if len(answer_out) < budget_places_count:
             passing_score = 0
-        elif answer_out[budget_places_count - 1]['eval_sum'] == answer_out[budget_places_count]['eval_sum']:
+        else:
+            passing_score = answer_out[budget_places_count]['eval_sum']
+
+        if answer_out[budget_places_count]['eval_sum'] == answer_out[budget_places_count + 1]['eval_sum']:
             passing_score = 1
         else:
-            passing_score = answer_out[budget_places_count - 1]['eval_sum']
-
-
-
+            passing_score = answer_out[budget_places_count]['eval_sum']
 
         # send answer to console
         print(passing_score)
 
         # make txt file for save answer to txt
-        string_data = "passing_score {}\nparticipants_count {}\nbudget_places_count {}\n".format(
+        string_data = "passing_score '{}'\nparticipants_count '{}'\nbudget_places_count {}\n".format(
             passing_score, participants_count, budget_places_count
         )
         string_data += '=======Бюджет!=======\n'
 
         for el in answer_out:
-            # list to string
-            el_name = ' '.join(el['name'])
-            el_eval = ' '.join(el['eval'])
-
-            string_data += "{} | {} | {}\n".format(el_name, el_eval, el['eval_sum'])
-            if el == answer_out[budget_places_count - 1]:
+            string_data += "'{}' '{}' '{}'\n".format(el['name'], el['eval'], el['eval_sum'])
+            if el == answer_out[budget_places_count]:
                 string_data += '=====================\n'
 
-        cur_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        cur_date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M")
         data_write_to_txt('data{}.txt'.format(cur_date), string_data)
 
 # read from file
